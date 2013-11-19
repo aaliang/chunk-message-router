040dd0e32d07445d281ae80507d2c8a60d80c24f
Andy Liang <a.liang1989@gmail.com>
2013-11-19 10:37:07 -0500

    Update MessageRelay.py

--- utils/MessageRelay.py
+++ utils/MessageRelay.py
@@ -1,12 +1,19 @@
 from types import IntType, StringType
 
 class MessageRelay(object):
+    """
+        Represents a type of message relay router. Each message relay type has a name, a subnet mask, and a throughput
+    """
+    
     
     def __init__(self, relay_name, subnet_prefix, throughput):
         """
             @type relay_name: StringType
             @type subnet_prefix: StringType
             @type throughput: IntType
+            
+            @param subnet_prefix: CIDR notation of a subnet mask
+            @param throughput: The number of chunks the relay is capabale of passing through
         """
         assert isinstance(relay_name, StringType)
         assert isinstance(subnet_prefix, StringType)
